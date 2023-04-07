43c5a882ee776fbb2a629dcab32dc717df49150e
Thiago Luiz <110872096+ThiagoDevLuiz@users.noreply.github.com>
2023-04-07 15:01:12 -0300

    Tentativa de carrossel(exclui tudo)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
-
+import Carrossel from './components/Carrossel/Carrossel';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
-        
+        <Carrossel />
       </main>
     </div>
   );
---
+++ src/components/Carrossel/Carrossel.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Carrossel.module.css'
+
+const Carrossel = () => {
+  return (
+    <div className={styles.teste}></div>
+  )
+}
+
+export default Carrossel
